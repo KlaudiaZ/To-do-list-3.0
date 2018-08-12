01696eedacf535fa43ec5d376dc1fb4c8abb40c0
KlaudiaZ <klaudia.zajac@email.com>
2018-08-12 12:44:11 +0200

    Bind edit and remove

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import { getTasks } from './modules/getTasks';
+import handle from './handlers';
 
 export const init = () => {
-    console.log('Initializing...')
-    getTasks();
+    console.log('Initializing...');
+
+    getTasks().then((taskList) => {
+        bindEditTaskButton(taskList);
+        bindRemoveTaskButton(taskList);
+    });
+}
+
+const bindEditTaskButton = (taskList: object) => {
+    const btns = document.querySelectorAll('.edit');
+    btns.forEach((btn) => {
+        btn.addEventListener('click', function () {
+            const id: number = this.getAttribute('data-id');
+            handle.editTask(id, taskList);
+        });
+    });
+}
+
+const bindRemoveTaskButton = (taskList: object) => {
+    const btns = document.querySelectorAll('.remove');
+    btns.forEach((btn) => {
+        btn.addEventListener('click', function () {
+            const id: number = this.getAttribute('data-id');
+            handle.removeTask(id, taskList);
+        });
+    });
 }
